9394ab6609da5b92d6119e8aff05e1ee02573520
cryptic Dev <oluwagbemigagbangbola@gmail.com>
2025-10-30 15:50:43 +0100

    Revert "profile picture"

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,29 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 // Path: next.config.js
 const nextConfig: NextConfig = {
   webpack: (config) => {
-    config.externals.push("pino-pretty", "lokijs", "encoding");
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
     return config;
   },
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "unavatar.io",
-      },
-      {
-        protocol: "https",
-        hostname: "pbs.twimg.com",
-      },
-      {
-        protocol: "https",
-        hostname: "abs.twimg.com",
-      },
-    ],
   },
 };
 

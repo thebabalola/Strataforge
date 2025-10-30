6197fbeef6a2ab19b6c2bf6beb3bd1857a7294f3
oluwagbemiga <oluwagbemigagbangbola@gmail.com>
2025-10-30 15:47:29 +0100

    refactor(next.config): Standardize quotation marks in configuration file

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,9 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 // Path: next.config.js
 const nextConfig: NextConfig = {
   webpack: (config) => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
   },
   eslint: {
@@ -12,16 +12,16 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'unavatar.io',
+        protocol: "https",
+        hostname: "unavatar.io",
       },
       {
-        protocol: 'https',
-        hostname: 'pbs.twimg.com',
+        protocol: "https",
+        hostname: "pbs.twimg.com",
       },
       {
-        protocol: 'https',
-        hostname: 'abs.twimg.com',
+        protocol: "https",
+        hostname: "abs.twimg.com",
       },
     ],
   },
